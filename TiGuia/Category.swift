24196ba181a1a1e441e450ae33b2a4a66f3f712d
Dara <dcsv@cin.ufpe.br>
2020-11-20 17:30:22 -0300

    ëstrutura de classes

--- TiGuia/Category.swift
+++ TiGuia/Category.swift
@@ -14,16 +14,19 @@ class Category {
     var content:String
     var image:Image?
     var links:[Link]
+    var subcategory:[Subcategory]
     
     init() {
         self.title = ""
         self.content = ""
         self.links = []
+        self.subcategory = []
     }
     
-    init(title:String,content:String,links:[Link]) {
+    init(title:String,content:String,links:[Link],subcategory:[Subcategory]) {
         self.title = title
         self.content = content
         self.links = links
+        self.subcategory = subcategory
     }
 }
