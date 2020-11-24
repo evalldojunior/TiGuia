db68d2c93153eb1220bbf745c067bb1173605b4f
Palloma Ramos <pallomaramos@MacBook-Pro-de-Palloma.local>
2020-11-24 17:57:14 -0300

    Chat visualmente pronto

--- ChatUI/ChatUI.swift
+++ ChatUI/ChatUI.swift
@@ -30,6 +30,7 @@ struct ChatUI : View {
         config.filter = .images //videos, livePhotos...
         config.selectionLimit = 0 //0 => any, set 1-2-3 for hard limit
         return config
+        
     }
     func Scroll(reader :ScrollViewProxy) -> some View {
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.05) {
@@ -41,61 +42,81 @@ struct ChatUI : View {
     }
     
     var body: some View {
-        HStack {
-            Button(action: {
-                isPickerPresented.toggle()
-            }) {
-                Image(systemName: "chevron.backward")}
-//            }.sheet(isPresented: $backButton) {
-//                PhotoPicker(configuration: self.config, pickerResult: { (imageArray) in
-//                    var index = ChatUI.pickerResult.count
-//                    for image in imageArray {
-//                        ChatUI.pickerResult.append(image)
-//                        messageData.append(MessageData(isMine: true, dataType: .image(imageIndex: index)))
-//                        index += 1
-//                    }
-//                }, isPresented: $isPickerPresented)
-//        NavigationView {
-            VStack(alignment: .leading) {
-                Spacer()
-                Spacer()
-                Spacer()
-                Spacer()
-                Spacer()
-                Spacer()
-                //                        Image(systemName: "person.circle.fill")
-                Text("Mentoria com Dara")
-                    .font(.custom("Raleway-Bold", size: 30))
-                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                    .padding(.horizontal)
-                Text("Professora de Computação")
-                    .font(.custom("Raleway-Regular", size: 20))
-                    .foregroundColor(.black)
-                    .padding(.horizontal)
+        let buttonColor = Color(red: 28/255, green: 118/255, blue: 144/255, opacity: 1.0)
+        let lightColor = Color(red: 252/255, green: 252/255, blue: 252/255, opacity: 1.0)
+        let orangeColor = Color(red: 251/255, green: 153/255, blue: 28/255, opacity: 1.0)
+        let blackColor = Color(red: 32/255, green: 34/255, blue: 38/255, opacity: 1.0)
+        let lightBlueColor = Color(red: 33/255, green: 158/255, blue: 188/255, opacity: 1.0)
+        let messageBlueColor = Color(red: 31/255, green: 145/255, blue: 173/255, opacity: 1.0)
+        let greyColor = Color(red: 224/255, green: 230/255, blue: 236/255, opacity: 1.0)
+        let lightGreyColor = Color(red: 247/255, green: 249/255, blue: 250/255, opacity: 1.0)
+        
+        VStack{
+        Button(action: {
+//
+        }) {
+            Image(systemName: "chevron.backward")
+                .foregroundColor(buttonColor)
+                .padding(.trailing, 370.0)
+        }
+        
+        VStack(alignment: .leading) {
+//            Spacer()
+//            Spacer()
+            Spacer()
+            Spacer()
+            Spacer()
+            Spacer()
+            Text("Mentoria com Dara")
+                .font(.custom("Raleway-Bold", size: 30))
+                .foregroundColor(lightBlueColor)
+                .padding(.horizontal)
+            Text("Professora de Computação")
+                .font(.custom("Raleway-Regular", size: 18))
+                .foregroundColor(blackColor)
+                .padding(.horizontal)
+            
+            VStack {
+                Text("Hoje")
+                    .multilineTextAlignment(.center)
+                    .background(lightColor)
+                    .cornerRadius(3)
+                    .padding(.top)
+                    .padding(.leading,25)
+                    .font(.custom("Raleway-Regular", size: 17))
+                    .foregroundColor(blackColor)
                 
+                VStack {
+                    
+                    Text("Essa conversa é sobre Segurança da Informação")
+                        .multilineTextAlignment(.center)
+                        .background(lightGreyColor)
+                        .cornerRadius(3)
+                        .padding(.top)
+                        .padding(.leading,40)
+                        .font(.custom("Raleway-Regular", size: 18))
+                        .foregroundColor(blackColor)
+                    
+                }
+                
+                
+            }
             ZStack {
-                //                                VStack(alignment: .leading) {
-                //                                    //                        Image(systemName: "person.circle.fill")
-                //                                    Text("Mentoria com Dara")
-                //                                        .font(.custom("Raleway-Bold", size: 30))
-                //                                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                //                                    Text("Professora de Computação")
-                //                                        .font(.custom("Raleway-Regular", size: 20))
-                //
-                //
-                //                                }
                 ScrollView(.vertical, showsIndicators: false) {
                     ScrollViewReader { reader in
                         LazyVStack(spacing: 0.5) {
                             ForEach(messageData) {  message in
                                 ChatBubble(direction: message.isMine ? .right : .left) {
                                     switch message.dataType {
-                                    case .text(let userMessage):
+                                    case
+                                        .text(let userMessage):
                                         Text(userMessage)
-                                            .padding(.all, 10)
-                                            .foregroundColor(Color.white)
-                                            .background(message.isMine ? Color.blue : Color.gray)
-                                    case .image(let imageIndex):
+                                            .padding(.all, 15)
+                                            .foregroundColor(message .isMine ? lightColor :  blackColor)
+                                            .font(.custom("Raleway-Regular", size: 17))
+                                            .background(message.isMine ? messageBlueColor : greyColor)
+                                    case
+                                        .image(let imageIndex):
                                         Image(uiImage: ChatUI.pickerResult[imageIndex])
                                             .resizable()
                                             .frame(width: UIScreen.main.bounds.width - 70,
@@ -113,68 +134,42 @@ struct ChatUI : View {
                 VStack {
                     Spacer()
                     HStack {
-                        Button(action: {
-                            isPickerPresented.toggle()
-                        }) {
-                            //                                Image(systemName: "plus")
-                        }.sheet(isPresented: $isPickerPresented) {
-                            PhotoPicker(configuration: self.config, pickerResult: { (imageArray) in
-                                var index = ChatUI.pickerResult.count
-                                for image in imageArray {
-                                    ChatUI.pickerResult.append(image)
-                                    messageData.append(MessageData(isMine: true, dataType: .image(imageIndex: index)))
-                                    index += 1
-                                }
-                            }, isPresented: $isPickerPresented)
-                            
-                            
-                        }
                         TextField("Mensagem", text: $fullText)
                             .frame(width: UIScreen.main.bounds.width - 70)
                             .textFieldStyle(RoundedBorderTextFieldStyle())
-                            .border(Color.blue)
+                            .font(.custom("Raleway-Regular", size: 17))
+                            .border(lightBlueColor)
                             .cornerRadius(3)
                             .padding(.trailing)
                         Spacer()
                             .frame(width: -10.0)
-                            
+                        
                         Button(action: {
                             messageData.append(MessageData.init(isMine: true, dataType: .text(message: fullText)))
-                            messageData.append(MessageData.init(isMine: false, dataType: .text(message: "reply to:" + fullText)))
+                            messageData.append(MessageData.init(isMine: false, dataType: .text(message: "reply to: " + fullText)))
                             fullText = ""
                         }) {
                             Image(systemName: "paperplane")
-                                .foregroundColor(.white)
+                                .foregroundColor(lightColor)
                                 .padding(.all, 6)
-                                .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
+                                .background(buttonColor)
                                 .cornerRadius(3)
                             
                         }.disabled(fullText.isEmpty)
                     }
-                    .padding([.leading, .trailing], 20)
+                    .padding([.leading, .trailing], 10)
                     .padding([.top, .bottom], 10)
-//                    .background(Color(red: 0.9, green: 0.9, blue: 0.9))
+                    .background(lightColor)
                     
                 }
             }
             .padding(.top)
             
+            
         }
-        //            .navigationBarTitleDisplayMode(.inline)
-        //            .toolbar(content: {
-        //                ToolbarItem(placement: .principal) {
-//        HStack {
-//            //                        Image(systemName: "person.circle.fill")
-//            Text("Mentoria com Dara")
-//                .font(.custom("Raleway-Bold", size: 30))
-//                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-//        }
     }
 }
-//)
-//}
-    }
-//}
+}
 
 struct ChatUI_Previews: PreviewProvider {
     static var previews: some View {
