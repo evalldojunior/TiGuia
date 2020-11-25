a4109e244df4aa33857c10b20296436737e75c44
Meyrillan <meyrillan@gmail.com>
2020-11-25 17:13:28 -0300

    Class Favorite added

--- TiGuia/Favorite.swift
+++ TiGuia/Favorite.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import SwiftUI
+
+class Favorite {
+    
+    var subcategories: [Subcategory]
+    var subcategory: Subcategory
+    
+    init() {
+        subcategories = []
+        subcategory = Subcategory()
+    }
+    
+    func returnSubcategories() -> [Subcategory] {
+        return self.subcategories
+    }
+    
+    func addSubcategory(subcategory: Subcategory) {
+        self.subcategories.append(subcategory)
+    }
+}
