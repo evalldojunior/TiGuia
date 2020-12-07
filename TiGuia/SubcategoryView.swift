1999e1adcc1c379db0f2a59dc1db5d76e16bd538
evalldojunior <evalldojn@gmail.com>
2020-12-07 14:59:51 -0300

    ajustes na tela de onboarding, categoria e subcategoria

--- TiGuia/SubcategoryView.swift
+++ TiGuia/SubcategoryView.swift
@@ -25,121 +25,138 @@ public struct SubcategoryView: View {
     
     public var body: some View {
         
-        
-        VStack {
-            //
-            //MARK: -Header - titulo + botao de favoritos
-            //
-            HStack {
-                //título
-                Text(category.title)
-                    .foregroundColor(.titleColor)
-                    .font(.custom("Raleway-Bold", size: 30))
-                    .multilineTextAlignment(.leading)
+        GeometryReader { geometry in
+            ScrollView {
+                GeometryReader { gmt in
+                    Rectangle().fill(Color.gray.opacity(0.5))
+                        .frame(height: geometry.size.height, alignment: .center) // aqui antes era  geometry.size.height / 4 + 30
+                        .edgesIgnoringSafeArea(.top)
+                        .offset(y: gmt.frame(in: .global).minY > 0 ? -gmt.frame(in: .global).minY : 0)
+                }.frame(height: geometry.size.height / 4 + 30)
                 
-                Spacer()
-                
-                Button(action: {
-                    self.favorito.toggle()
-                    if (self.favorito) {
-                        Data.favorite.addSubcategory(subcategory: category)
-                    } else {
-                        Data.favorite.removeSubcategory(subcategory: category)
-                    }
-                    category.favorite = favorito
-                    
-                }, label: {
-                    Image(systemName: self.favorito == true ? "star.fill" : "star")
-                        .resizable()
-                        .scaledToFit()
-                        .padding(10)
-                        .background(Color.btnColor)
-                        .foregroundColor(.lightColor)
-                        .frame(width: 48, height: 48, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                        .cornerRadius(10)
-                })
-                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-                .offset(y: -10) // realmente necessario?
-                
-            }.padding()
-            
-            //
-            //MARK: -inicio do conteúdo
-            //
-            
-            ScrollView{
                 VStack {
-                    Text(category.content)
-                        .font(.custom("Raleway-Regular", size: 15))
-                        .multilineTextAlignment(.leading)
-                        //.padding()
-                        .foregroundColor(.darkColor)
-                        .fixedSize(horizontal: false, vertical: true)
-                }.padding()
-                
-                HStack{
-                    Text("Links úteis")
-                        .multilineTextAlignment(.leading)
-                        .font(.custom("Raleway-Bold", size: 20))
-                        .foregroundColor(.titleColor)
-                    Spacer()
-                }.padding([.top, .leading, .trailing])
-                
-                SubCardLink(category: category)
-                
-                if !category.subcategories.isEmpty {
-                    HStack{
-                        Text("Categorias")
-                            .font(.custom("Raleway-Bold", size: 20))
+                    //
+                    //MARK: -Header - titulo + botao de favoritos
+                    //
+                    HStack {
+                        //título
+                        Text(category.title)
                             .foregroundColor(.titleColor)
-                        Spacer()
-                    }.padding([.top, .leading, .trailing])
-                    
-                    VStack {
-                        LazyVStack {
-                            ForEach(0..<category.subcategories.count, id: \.self) { count in
-                                SubCardsCategory(category: category, count: count)
-                            }
-                        }
-                    }.padding()
-                } else {
-                    Spacer(minLength: 30)
-                }
-                
-                VStack {
-                    Button(action: {
-                        self.presented.toggle()
-                    }, label: {
-                        Spacer()
-                        Image(systemName: "ellipses.bubble")
-                            .resizable()
-                            .scaledToFit()
-                            .foregroundColor(.lightColor)
-                            .frame(width: 20, height: 20, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                        Text("Pedir ajuda")
-                            .font(.custom("Raleway-Bold", size: 18))
-                            .foregroundColor(.lightColor)
+                            .font(.custom("Raleway-Bold", size: 30))
+                            .multilineTextAlignment(.leading)
+                        
                         Spacer()
                         
-                    }).padding()
-                    .clipped()
-                    .background(Color.btnColor)
-                    .cornerRadius(10)
-                    .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-                    .fullScreenCover(isPresented: $presented, content: {
-                        //HelpUI()
-                    })
-                }.padding()
+                        Button(action: {
+                            self.favorito.toggle()
+                            if (self.favorito) {
+                                Data.favorite.addSubcategory(subcategory: category)
+                            } else {
+                                Data.favorite.removeSubcategory(subcategory: category)
+                            }
+                            category.favorite = favorito
+                            
+                        }, label: {
+                            Image(systemName: self.favorito == true ? "star.fill" : "star")
+                                .resizable()
+                                .scaledToFit()
+                                .padding(10)
+                                .background(Color.btnColor)
+                                .foregroundColor(.lightColor)
+                                .frame(width: 48, height: 48, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                .cornerRadius(10)
+                        })
+                        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                        .offset(y: -10) // realmente necessario?
+                        
+                    }.padding()
+                    
+                    //
+                    //MARK: -inicio do conteúdo
+                    //
+                    
+                    ScrollView{
+                        VStack {
+                            Text(category.content)
+                                .font(.custom("Raleway-Regular", size: 15))
+                                .multilineTextAlignment(.leading)
+                                //.padding()
+                                .foregroundColor(.darkColor)
+                                .fixedSize(horizontal: false, vertical: true)
+                        }.padding()
+                        
+                        HStack{
+                            Text("Links úteis")
+                                .multilineTextAlignment(.leading)
+                                .font(.custom("Raleway-Bold", size: 20))
+                                .foregroundColor(.titleColor)
+                            Spacer()
+                        }.padding([.top, .leading, .trailing])
+                        
+                        SubCardLink(category: category)
+                        
+                        if !category.subcategories.isEmpty {
+                            HStack{
+                                Text("Categorias")
+                                    .font(.custom("Raleway-Bold", size: 20))
+                                    .foregroundColor(.titleColor)
+                                Spacer()
+                            }.padding([.top, .leading, .trailing])
+                            
+                            VStack {
+                                LazyVStack {
+                                    ForEach(0..<category.subcategories.count, id: \.self) { count in
+                                        SubCardsCategory(category: category, count: count)
+                                    }
+                                }
+                            }.padding()
+                        } else {
+                            Spacer(minLength: 30)
+                        }
+                        
+                        VStack {
+                            Button(action: {
+                                self.presented.toggle()
+                            }, label: {
+                                Spacer()
+                                Image(systemName: "ellipses.bubble")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .foregroundColor(.lightColor)
+                                    .frame(width: 20, height: 20, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                Text("Pedir ajuda")
+                                    .font(.custom("Raleway-Bold", size: 18))
+                                    .foregroundColor(.lightColor)
+                                Spacer()
+                                
+                            }).padding()
+                            .clipped()
+                            .background(Color.btnColor)
+                            .cornerRadius(10)
+                            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                            .fullScreenCover(isPresented: $presented, content: {
+                                //HelpUI()
+                            })
+                        }.padding()
+                        
+                        Spacer(minLength: 20)
+                        
+                    }
+                    .navigationBarTitle("", displayMode: .inline)
+                    //.navigationTitle(Text(""))
+                    //.navigationBarHidden(true)
+                    .statusBar(hidden: true)
+                    
+                    
+                }.clipped()
+                .background(Color.backgroundColor) // ta mostrando o fundo de cores diferentes
+                .cornerRadius(25, corners: [.topLeft, .topRight])
+                .offset(x: 0, y: -35)
+                .shadow(color: .init(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.4), radius: 15, x: 0.0, y: -5.0)
                 
-                Spacer(minLength: 20)
-                
-            }
-            .navigationBarTitle("", displayMode: .inline)
-            //.navigationTitle(Text(""))
-            //.navigationBarHidden(true)
-            .statusBar(hidden: true)
+            }.edgesIgnoringSafeArea(.top)
             
-        }//.edgesIgnoringSafeArea(.top)
+        }
         
     }
     
